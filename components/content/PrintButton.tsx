b8336b3834d00e0f6ca019f30241d2c864e11c4d
anchitjaincfa <anchit.jain@berkeley.edu>
2026-09-21 23:23:51 -0700

    Decouple print control styles from server component

--- components/content/PrintButton.tsx
+++ components/content/PrintButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { styles } from "./ContentShell";
+import styles from "./content.module.css";
 
 export function PrintButton() {
   return <button className={styles.printButton} type="button" onClick={() => window.print()}>Print this guide</button>;
